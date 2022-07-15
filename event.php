d2b73cec68c1039dc0b091bf026a8bb05ce23cf4
Akifffff <abdakif12@gmail.com>
2022-07-15 22:48:51 +0800

    commit

---
+++ event.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>VIRTUAL RUN EVENT</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body style="background-color:MediumSeaGreen;">
+    EVENT DETAILS <br>
+    DATE : 27/7/2022 <br>
+    DISTANCE OF RUNNING PATH : 5 KM<br>
+    EVENT THEME : NEON COLOUR<br>
+    REGISTRATION DATELINE : 20/7/2022<br>
+    PLACE : STADIUM KUALA TERENGGANU<br>
+</body>
